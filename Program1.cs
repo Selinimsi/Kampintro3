6dcaee4169b2909a25aa67e84cb7d672d629a980
selin yıldırım <sy248359@gmail.com>
2025-02-05 11:25:59 +0300

    Add project files.

---
+++ Program1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kampintro3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety: tip güvenliği-> tanımladığın değişkenin türünü belirtmek zorundasın.
+            string degisken = "merhaba";
+            
+
+            
+
+
+
+
+            Console.WriteLine("merhaba dünya");
+            Console.ReadLine();
+        }
+    }
+}
